3d4fffefbb6185e8b27953292135e146e5afc862
Krzysztof Mikoszewski <krzysztof.mikoszewski@dka.io>
2024-06-25 12:41:40 +0200

    top_toolbar-add-links

--- app/content/components/Toolbar/TopToolbar.tsx
+++ app/content/components/Toolbar/TopToolbar.tsx
@@ -12,36 +12,52 @@ export const TopToolbar = () => {
   };
   return (
     <div className="bg-transparent p-4 flex justify-between items-center sticky top-0 h-[80px]">
-      <div className="flex space-x-4 p-2">
-        <button
-          className="button-toolbar"
-          onClick={() => scrollToSection("offer")}
-        >
-          oferta
-        </button>
-        <Link href={"/o_nas"} className="button-toolbar">
-          o nas
+      <div className="flex justify-between space-x-4 p-2 w-1/2">
+        <div className=" flex gap-4">
+          <button
+            className="button-toolbar"
+            onClick={() => scrollToSection("offer")}
+          >
+            oferta
+          </button>
+          <Link href={"/o_nas"} className="button-toolbar">
+            o nas
+          </Link>
+          <button
+            className="button-toolbar"
+            onClick={() => scrollToSection("footer")}
+          >
+            kontakt
+          </button>
+          <button
+            className="button-toolbar"
+            onClick={() => scrollToSection("events")}
+          >
+            repertuar
+          </button>
+        </div>
+        <div className="flex gap-4">
+          <Link
+            href={"https://www.instagram.com/irga.impro/reels/"}
+            className="button-toolbar"
+            target={"_blank"}
+          >
+            instagram
+          </Link>
+          <Link
+            href={"https://www.facebook.com/irga.impro"}
+            target={"_blank"}
+            className="button-toolbar"
+          >
+            facebook
+          </Link>
+        </div>
+      </div>
+      <div className="justify-end">
+        <Link href="/">
+          <Image src={logo} alt={"logo"} />
         </Link>
-        <button
-          className="button-toolbar"
-          onClick={() => scrollToSection("footer")}
-        >
-          kontakt
-        </button>
-        <button
-          className="button-toolbar"
-          onClick={() => scrollToSection("events")}
-        >
-          repertuar
-        </button>
       </div>
-      <div>
-        <button className="button-toolbar">instagram</button>
-        <button className="button-toolbar">facebook</button>
-      </div>
-      <Link href="/">
-        <Image src={logo} alt={"logo"} />
-      </Link>
     </div>
   );
 };
